80e296eca41f6dc1d857f32ee709e25d2e6acb6d
admin <d.art.shishkin@gmail.com>
2020-07-19 12:07:01 +0300

    Initial commit

---
+++ src/main/java/com/artarkatesoft/videoplayer/VideoPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.videoplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPlayerApplication.class, args);
+    }
+
+}
