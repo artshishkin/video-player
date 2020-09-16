ae797ed363446eff1ee38f79c5fe8efd1cfb6591
Art <d.art.shishkin@gmail.com>
2020-09-16 12:03:16 +0300

    added full content and partial content work fine

--- src/main/java/com/artarkatesoft/videoplayer/controllers/VideoController.java
+++ src/main/java/com/artarkatesoft/videoplayer/controllers/VideoController.java
@@ -1,18 +1,16 @@
 package com.artarkatesoft.videoplayer.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.MediaTypeFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.net.MalformedURLException;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("videos")
@@ -21,14 +19,48 @@ public class VideoController {
     @Value("${videoLocation}")
     private String videoLocation;
 
+    public static final long ChunkSize = 1000000L;
+
     @GetMapping("{name}/full")
-    public ResponseEntity<FileSystemResource> getFullVideo(@PathVariable String name) throws MalformedURLException {
-        FileSystemResource video = new FileSystemResource(videoLocation + "/" + name);
-//        UrlResource video = new UrlResource("file:///" + videoLocation + "/" + name);
+    @ResponseBody
+    public ResponseEntity<UrlResource> getFullVideo(@PathVariable String name) throws IOException {
+//        FileSystemResource video = new FileSystemResource(videoLocation + "/" + name);
+        UrlResource video = new UrlResource("file:" + videoLocation + "/" + name);
         return ResponseEntity
-                .status(HttpStatus.PARTIAL_CONTENT)
+                .ok()
+//                .status(HttpStatus.PARTIAL_CONTENT)
                 .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
                 .body(video);
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<ResourceRegion> getVideo(@PathVariable String name, @RequestHeader HttpHeaders headers) throws IOException {
+        UrlResource video = new UrlResource("file:" + videoLocation + '/' + name);
+        ResourceRegion region = resourceRegion(video, headers);
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(video)
+                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(region);
+    }
+
+
+    private ResourceRegion resourceRegion(UrlResource video, HttpHeaders headers) throws IOException {
+        long contentLength = video.contentLength();
+        List<HttpRange> headersRange = headers.getRange();
+        HttpRange range = headersRange.iterator().hasNext() ? headersRange.iterator().next() : null;
+        long start;
+        ResourceRegion resourceRegion;
+        if (range != null) {
+            start = range.getRangeStart(contentLength);
+            long end = range.getRangeEnd(contentLength);
+            long rangeLength = Long.min(1000000L, end - start + 1L);
+            resourceRegion = new ResourceRegion((Resource) video, start, rangeLength);
+        } else {
+            start = Long.min(1000000L, contentLength);
+            resourceRegion = new ResourceRegion((Resource) video, 0L, start);
+        }
+        return resourceRegion;
     }
 
     @GetMapping
